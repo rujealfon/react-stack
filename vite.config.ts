51d08797a5dc35dd405879a9d8f5243c4ae3040a
Ruje Alfon <7766205+rujealfon@users.noreply.github.com>
2025-05-03 23:08:26 +0800

    Add React Compiler plugin and upgrade react-hooks ESLint plugin

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,18 @@ import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 
+// https://vite.dev/config/
+const ReactCompilerConfig = {};
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    TanStackRouterVite({ autoCodeSplitting: true }),
-    viteReact(),
+    TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
+    viteReact({
+      babel: {
+        plugins: [['babel-plugin-react-compiler', ReactCompilerConfig]],
+      },
+    }),
     tailwindcss(),
   ],
   test: {
